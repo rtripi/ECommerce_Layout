0da4b4ff75601062485e7d0bd083bdcf90f2735e
Renato Tripi <renatosft@gmail.com>
2021-10-29 20:35:27 -0300

    single product and cart page layout created

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const menuBtn = document.querySelector(".menu-icon");
 const menu = document.querySelector("nav > ul");
 
+let productImg = document.querySelector("#product-img");
+const smallImg = document.querySelectorAll(".small-img");
+
+//Toggle menu function
 const toggleMenu = () => {
   menu.classList.toggle("active");
 };
 
+//Events Listeners
 menuBtn.addEventListener("click", toggleMenu);
+smallImg.forEach((img) =>
+  img.addEventListener("click", () => {
+    productImg.src = img.src;
+  })
+);
